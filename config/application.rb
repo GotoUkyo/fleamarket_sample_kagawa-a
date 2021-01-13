c0091ab2096828e7102d652bcd9f79dd3ffb4536
KosukeNogo <kousuke.nogo@gmail.com>
2021-01-13 22:34:52 +0900

    basic認証の実装

--- config/application.rb
+++ config/application.rb
@@ -17,15 +17,3 @@ module FleamarketSampleKagawaA
     # the framework and any gems in your application.
   end
 end
-
-#野郷が追加：basic認証関連：2021-01-13
-before_action :basic_auth
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
-    end
-  end
