72a0f56f88fac31dc64904e89080a532f29a7f00
KosukeNogo <kousuke.nogo@gmail.com>
2021-01-25 00:11:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'indexes#index'
+  resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
