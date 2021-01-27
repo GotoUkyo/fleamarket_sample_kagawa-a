7da568a2e35344e9266a825ee0cc95b564fdee9a
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2021-01-27 19:52:08 +0900

    アドレステーブル読み込み
    accepts_nested_attributes_forメソッドを使って２つのレコードをまとめて登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nick_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday ,presence: true
   has_one :address
+  accepts_nested_attributes_for :address
 end
