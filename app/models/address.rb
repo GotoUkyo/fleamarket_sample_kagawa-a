aae2db46529fd5dfde9e86827013d2587154a616
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2021-01-28 13:08:03 +0900

    active hash 導入
    都道府県の選択にactive hash使用

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :postcode, :prefecture_id, :city, :block ,presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  validates :postcode, :prefecture, :city, :block ,presence: true
 end
