32e1bfba7be0e1727ad83f556746c644ee67c507
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2021-01-29 12:23:43 +0900

    バリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,9 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   # validates :postcode, :prefecture, :city, :block ,presence: true
+  validates :postcode, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :block, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # validates :nick_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+  validates :nick_name, presence: true
+  validates :email, presence: true, uniqueness:true,
+            format: { with: /\A\S+@\S+\.\S+\z/}
+  validates :password, presence: true, length:{ minimum: 7 }, confirmation: true
+  validates :last_name, presence: true,
+            format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+            message: "全角で入力して下さい"}
+  validates :first_name, presence: true,
+            format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+            message: "全角で入力して下さい"}
+  validates :last_name_kana, presence: true,
+            format: { with: /\A[ァ-ヶー－]+\z/,
+            message: "全角カナで入力して下さい"}
+  validates :first_name_kana, presence: true,
+            format: { with: /\A[ァ-ヶー－]+\z/,
+            message: "全角カナで入力して下さい"}
+  validates :birthday, presence: true
   has_one :address
   accepts_nested_attributes_for :address
 end
