03cd6fafcfb0a0d3757306dd91b39b6ef7fd2b29
GotoUkyo <itabiyo@yahoo.co.jp>
2021-01-30 13:31:29 +0900

    画像投稿用のビューを設定
    ・最上部ロゴを設定
    ・画像を投稿する部分の枠、アイコン、説明文等を設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'indexes#index'
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items
 end
