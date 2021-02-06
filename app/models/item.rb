4a584137aa991534fef10f1feae10f9ed3d84109
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2021-02-06 12:01:54 +0900

    activehash
    postage,day,stateのビューとmodel

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to:user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :state
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :day
 end
