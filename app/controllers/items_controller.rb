15c89d7a1d758b3b1fc0143aa9aa2144633c3d35
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2021-02-06 15:57:33 +0900

    パラーメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,5 +10,11 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
+  end
+  
+  private
+  def item_params
+    params.reqire(:item).permit(:name,:description,:brand,:state_id,:postage_id,:area_id,:day_id,:price)
   end
 end
