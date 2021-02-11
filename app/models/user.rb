df726779f329b7cf20850e1b627b79c480911fd2
KosukeNogo <kousuke.nogo@gmail.com>
2021-02-12 01:30:19 +0900

    user.rbアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
     validates :birthday
   end
+  has_one :credit
   has_one :address
   accepts_nested_attributes_for :address
+  
 end
