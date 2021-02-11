87d6cd72205357df4560257708bd3d09a63d8961
KosukeNogo <kousuke.nogo@gmail.com>
2021-02-12 01:32:24 +0900

    itemsコントローラ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,29 @@
 class ItemsController < ApplicationController
+
+  require 'payjp' #これでpayjpのメソッドが使用できます
+
   def index
   end
 
   def show
+    @item = Item.find(2) # @item = Item.find(params[:id])←最終的にこのコードに書き換えます。
+    @address = Address.find(current_user.id)
   end
 
   def new
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(
+      :postcode,
+      :prefecture_id,
+      :city,
+      :block,
+      :building,
+      :phone_number,
+      :user_id,
+    ).merge(user_id: current_user.id)
   end
 end
