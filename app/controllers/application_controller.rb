3b09207c59e91e9bde96aff5df53087bf100ccf9
KosukeNogo <kousuke.nogo@gmail.com>
2021-02-13 22:17:18 +0900

    users#showをindexes#showに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,12 +25,12 @@ class ApplicationController < ActionController::Base
   end
   # basic認証関連↑
 
-  def after_sign_in_path_for(resource)
-    root_path # ログイン後に遷移するpathを設定
-  end
+  # def after_sign_in_path_for(resource)
+  #   root_path # ログイン後に遷移するpathを設定
+  # end
   
-  def after_sign_out_path_for(resource)
-    root_path # ログアウト後に遷移するpathを設定
-  end
+  # def after_sign_out_path_for(resource)
+  #   root_path # ログアウト後に遷移するpathを設定
+  # end
 
 end
--- app/controllers/indexes_controller.rb
+++ app/controllers/indexes_controller.rb
@@ -3,4 +3,7 @@ class IndexesController < ApplicationController
   def index
   end
 
+  def show
+  end
+
 end
